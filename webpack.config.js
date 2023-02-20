801770e87ce3e973bf94583f5fdd6791656aed82
Viktor Bauer <bassa555@mail.ru>
2023-02-20 17:56:54 +0300

    homework modules-Import-Export for review

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: 'babel-loader',
+                },
+              },
+              {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+              },
+              {
+                test: /\.html$/,
+                use: {
+                    loader: 'html-loader',
+                },
+              },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: '.src/index.html',
+        }),
+        new MiniCssExtractPlugin()
+    ]
+}
+
+
